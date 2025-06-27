e541133d5e6b2f03538706e53e9498c1ddd2a1e5
Emart99 <ezequielsimon22@yahoo.com.ar>
2025-06-26 23:23:34 -0300

    spinner fix

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import { LoaderCircle } from "lucide-react";
 
 const LoadingSpinner = () => (
-    <div className="flex justify-center items-center py-24">
+    <div className="flex justify-center items-center py-24 min-h-screen">
         <LoaderCircle className="w-12 h-12 text-gray-800 dark:text-gray-200 animate-spin" />
     </div>
 );
