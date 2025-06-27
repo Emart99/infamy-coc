a0c22072595b1e0e4d1a3f058a701258ff2af022
Emart99 <ezequielsimon22@yahoo.com.ar>
2025-06-27 03:06:26 -0300

    changes to war details

--- src/components/WarDetailPage.jsx
+++ src/components/WarDetailPage.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { LoaderCircle, Star, Plus, Minus } from 'lucide-react';
 import { useParams } from 'react-router';
+import winnerCalculator from "../utils/winnerCalculator";
+
 
 const AttackColumn = ({ title, attack, opponentMembers }) => {
     if (!attack) {
@@ -88,7 +90,6 @@ const WarDetailPage = ({ warLog, currentWar }) => {
     } else {
         war = warLog.find(w => w.endTime === warId);
     }
-
     if (!war) {
         return (
             <div className="flex justify-center items-center h-screen">
@@ -96,19 +97,60 @@ const WarDetailPage = ({ warLog, currentWar }) => {
             </div>
         );
     }
-
-    const sortedClanMembers = [...war.clan.members].sort((a, b) => a.mapPosition - b.mapPosition);
-    const sortedOpponentMembers = [...war.opponent.members].sort((a, b) => a.mapPosition - b.mapPosition);
+    const opponent = war.clan.name != "iNFAMY" ? war.clan : war.opponent
+    const myClan = war.clan.name == "iNFAMY" ? war.clan : war.opponent
+    const result = winnerCalculator(myClan, opponent)
+    const resultEnemy = (text) =>{
+        if(text == "Victoria"){
+            return {text:'Derrota',color:'bg-red-100 text-red-800 dark:bg-red-900/50 dark:text-red-300'}
+        }
+        else if(text=="Derrota"){
+            return {text:'Victoria', color:'bg-green-100 text-green-800 dark:bg-green-900/50 dark:text-green-300'}
+        }
+        return{text:'Empate',color:'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300'};
+    }
+    
+    const sortedClanMembers = [...myClan.members].sort((a, b) => a.mapPosition - b.mapPosition);
+    const sortedOpponentMembers = [...opponent.members].sort((a, b) => a.mapPosition - b.mapPosition);
     console.log()
     return (
         <div className="p-4 md:p-8 bg-gray-200 dark:bg-gray-950 dark:border-gray-600 border-gray-300 border dark:text-gray-200 min-h-screen">
+            <div className="grid grid-cols-2 md:grid-cols-3 items-center w-full gap-2 md:gap-8 mb-6">
+                <div className="flex flex-col items-start">
+                    <div className="flex items-center gap-1 md:gap-2 mb-1 md:mb-2">
+                        <h1 className="text-lg md:text-4xl font-bold dark:text-white truncate">{myClan.name}</h1>
+                        <img src={myClan.badgeUrls.small} className="h-6 w-6 md:h-10 md:w-10 flex-shrink-0" alt="Badge mi clan" />
+                    </div>
+                    <div className="text-lg md:text-2xl lg:text-4xl font-bold text-yellow-500 flex items-center gap-1 md:gap-2">
+                        {myClan.stars}
+                        <Star className="fill-current w-4 h-4 md:w-6 md:h-6" />
+                    </div>
+                    <div className="text-gray-500 dark:text-gray-400 text-xs md:text-sm mt-1 hidden md:block">
+                        Estrellas (Nuestro Clan)
+                    </div>
+                </div>
+
+                <div className={`bg-gray-100 dark:bg-gray-800 rounded-lg p-2 md:p-6 justify-self-end md:justify-self-center  ${result.color}`}>
+                    <div className="text-lg md:text-2xl lg:text-4xl font-bold text-center">{result.text}</div>
+                </div>
+
+                <div className="flex flex-col items-end justify-self-end hidden md:flex ">
+                    <div className="flex items-center gap-1 md:gap-2 mb-1 md:mb-2">
+                        <img src={opponent.badgeUrls.small} className="h-6 w-6 md:h-10 md:w-10 flex-shrink-0" alt="Badge clan enemigo" />
+                        <h1 className="text-lg md:text-2xl lg:text-4xl font-bold dark:text-white truncate">{opponent.name}</h1>
+                    </div>
+                    <div className="text-lg md:text-4xl font-bold text-yellow-500 flex items-center gap-1 md:gap-2">
+                        {opponent.stars}
+                        <Star className="fill-current w-4 h-4 md:w-6 md:h-6" />
+                    </div>
+                    <div className="text-gray-500 dark:text-gray-400 text-xs md:text-sm mt-1 text-right hidden md:block">
+                        Estrellas (Clan Enemigo)
+                    </div>
+                </div>
+            </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div>
-                    <div className="flex justify-center gap-2">
-                        <img src={war.clan.badgeUrls.small} className="h-10 w-10" />
-                        <h1 className="text-4xl font-bold dark:text-white text-center pb-6">{war.clan.name}</h1>
-                    </div>
-
                     <div className="bg-gray-100 dark:bg-gray-900 p-4">
                         {sortedClanMembers.map(player => (
                             <PlayerCard
@@ -120,11 +162,27 @@ const WarDetailPage = ({ warLog, currentWar }) => {
                         ))}
                     </div>
                 </div>
+
                 <div>
-                    <div className="flex justify-center gap-2">
-                        <img src={war.opponent.badgeUrls.small} className="h-10 w-10" />
-                        <h1 className="text-4xl font-bold dark:text-white text-center pb-6">{war.opponent.name}</h1>
+                    <div className="flex justify-between items-center mb-6 md:hidden">
+                        <div className="flex flex-col justify-self-start   ">
+                            <div className="flex items-center gap-1 md:gap-2 mb-1 md:mb-2">
+                                <img src={opponent.badgeUrls.small} className="h-6 w-6 md:h-10 md:w-10 flex-shrink-0" alt="Badge clan enemigo" />
+                                <h1 className="text-lg md:text-4xl font-bold dark:text-white truncate">{opponent.name}</h1>
+                            </div>
+                            <div className="text-lg md:text-4xl font-bold text-yellow-500 flex items-center gap-1 md:gap-2">
+                                {opponent.stars}
+                                <Star className="fill-current w-4 h-4 md:w-6 md:h-6" />
+                            </div>
+                            <div className="text-gray-500 dark:text-gray-400 text-xs md:text-sm mt-1 text-right hidden md:block">
+                                Estrellas (Clan Enemigo)
+                            </div>
+                        </div>
+                        <div className={`bg-gray-100 dark:bg-gray-800 rounded-lg p-2 md:p-6 justify-self-end md:justify-self-center  ${resultEnemy(result.text).color}`}>
+                            <div className="text-lg md:text-2xl lg:text-4xl font-bold text-center">{resultEnemy(result.text).text}</div>
+                        </div>
                     </div>
+
                     <div className="bg-gray-100 dark:bg-gray-900 p-4">
                         {sortedOpponentMembers.map(player => (
                             <PlayerCard
